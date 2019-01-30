e7f37be52e6e0071f78cf82c5e55134bc4064fde
ianmagnone <ian.magnone@rover.com>
2019-01-30 13:45:54 -0800

    Trying new script.

--- demo.py
+++ demo.py
@@ -7,10 +7,7 @@ with picamera.PiCamera() as camera:
     camera.resolution = (2592, 1944)
     # Camera warm-up time
     time.sleep(2)
-    for filename in camera.capture_continuous('img{counter:03d}.jpg'):
-	    print('Captured %s' % filename)
-	    sleep(300) # wait 5 minutes
-    # camera.capture('/data/image.jpg')
+    camera.capture('/data/image.jpg')
 
 print 'Picture taken'
 time.sleep(10)
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-modprobe v4l2_common && python demo.py &
+modprobe v4l2_common && python timelapse.py &
 cd /data
 python -m SimpleHTTPServer 80
