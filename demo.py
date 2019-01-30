314588d6144c967b70507c559b122248b9f05ab4
ianmagnone <ian.magnone@rover.com>
2019-01-29 21:44:46 -0800

    First try at a timelapse sequence.

--- demo.py
+++ demo.py
@@ -7,7 +7,10 @@ with picamera.PiCamera() as camera:
     camera.resolution = (2592, 1944)
     # Camera warm-up time
     time.sleep(2)
-    camera.capture('/data/image.jpg')
+    for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+	    print('Captured %s' % filename)
+	    sleep(300) # wait 5 minutes
+    # camera.capture('/data/image.jpg')
 
 print 'Picture taken'
 time.sleep(10)
