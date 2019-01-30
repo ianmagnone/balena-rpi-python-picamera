d7a1866b34803dd2c0fb9534113b0d0bb1d92d83
ianmagnone <ian.magnone@rover.com>
2019-01-30 14:54:50 -0800

    Edit config filev14.

--- timelapse.py
+++ timelapse.py
@@ -7,8 +7,7 @@ from datetime import datetime
 from time import sleep
 import yaml
 
-# config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
-config = yaml.safe_load(open(data/config/ "config.yml"))
+config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
 image_number = 0
 
 
